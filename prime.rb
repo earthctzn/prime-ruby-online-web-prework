c7f8b49a133b9c904d1f3e898932c094bacd2d20
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 04:00:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,11 @@
-def prime?(number)
-  
+def prime?(n)
+  bool = true;
+  if n > 2 
+    arr = (2...num).to_a
+    arr.each do |x|
+      if num % x == 0 
+        bool = false 
+        break
+      end 
+    end
 end
